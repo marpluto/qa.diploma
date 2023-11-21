0174baa5e763c8955ea822e914e1af88c9534c81
Barni <maryonpluto@yandex.ru>
2023-11-21 08:16:35 +0300

    Update build.gradle

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -10,15 +10,14 @@ import java.sql.SQLException;
 
 public class SQLHelper {
     private static QueryRunner runner = new QueryRunner();
+    private static String url = System.getProperty("db.url");
 
     private SQLHelper() {
     }
 
     private static Connection getConn() throws SQLException {
 
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
-
-        //return DriverManager.getConnection("jdbc:postgresql://localhost:5432/app", "app", "pass");
+        return DriverManager.getConnection(url, "app", "pass");
     }
 
     @SneakyThrows
