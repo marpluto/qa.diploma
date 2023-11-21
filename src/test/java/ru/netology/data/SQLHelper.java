dfc5927154cb65bedb1c3b83c114b86d630efcca
Pluto <maryonpluto@yandex.ru>
2023-11-21 08:36:03 +0300

    Update build.gradle

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -11,13 +11,15 @@ import java.sql.SQLException;
 public class SQLHelper {
     private static QueryRunner runner = new QueryRunner();
     private static String url = System.getProperty("db.url");
+    private static String user = System.getProperty("db.user");
+    private static String password = System.getProperty("db.password");
 
     private SQLHelper() {
     }
 
     private static Connection getConn() throws SQLException {
 
-        return DriverManager.getConnection(url, "app", "pass");
+        return DriverManager.getConnection(url, user, password);
     }
 
     @SneakyThrows
